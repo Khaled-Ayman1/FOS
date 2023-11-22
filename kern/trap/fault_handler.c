e348a9a36d050fbe93b9131273a0eea9dd9f9355
itsnotyousef <yousefnasser266@yahoo.com>
2023-11-22 08:59:49 +0200

    PLACEMENT

--- kern/trap/fault_handler.c
+++ kern/trap/fault_handler.c
@@ -83,15 +83,42 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 		uint32 wsSize = env_page_ws_get_size(curenv);
 #endif
 
-	if(wsSize < (curenv->page_WS_max_size))
-	{
-		//cprintf("PLACEMENT=========================WS Size = %d\n", wsSize );
-		//TODO: [PROJECT'23.MS2 - #15] [3] PAGE FAULT HANDLER - Placement
-		// Write your code here, remove the panic and write your code
-		panic("page_fault_handler().PLACEMENT is not implemented yet...!!");
+		if(wsSize < (curenv->page_WS_max_size))
+			{
+				//cprintf("PLACEMENT=========================WS Size = %d\n", wsSize );
+				//TODO: [PROJECT'23.MS2 - #15] [3] PAGE FAULT HANDLER - Placement
+				// Write your code here, remove the panic and write your code
+				//panic("page_fault_handler().PLACEMENT is not implemented yet...!!");
+				 struct FrameInfo *f;
+				 uint32 faulted_page  = allocate_frame(&f);
+				 uint32 v_add = ROUNDDOWN(fault_va, PAGE_SIZE);
 
-		//refer to the project presentation and documentation for details
-	}
+				 uint32 page = pf_read_env_page(curenv, (void*) fault_va);
+				 if(page != E_PAGE_NOT_EXIST_IN_PF)
+				 {
+					 map_frame(curenv->env_page_directory, f, v_add, PERM_WRITEABLE | PERM_USER);
+				 }
+				 else{
+					 if ((fault_va < USTACKTOP && fault_va >= USER_HEAP_MAX) ||
+						 (fault_va < USER_HEAP_MAX && fault_va >= USER_HEAP_START))
+					 {
+							map_frame(curenv->env_page_directory, f, v_add, PERM_WRITEABLE | PERM_USER);
+
+					 }else{
+						 panic("rejected");
+					 }
+
+				 }
+				  struct WorkingSetElement* new_element = env_page_ws_list_create_element(curenv,fault_va);
+					if (new_element != NULL) {
+						if (curenv->page_last_WS_element == NULL) {
+							curenv->page_last_WS_element= new_element;
+						} else {
+							curenv->page_last_WS_element->prev_next_info.le_next = new_element;
+							curenv->page_last_WS_element = new_element;
+						}
+				}
+			}
 	else
 	{
 		//cprintf("REPLACEMENT=========================WS Size = %d\n", wsSize );
