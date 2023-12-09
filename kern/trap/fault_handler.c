03bea9b6144a07a7e02b39e661bc9e76149ab561
Hani <65543464+MHani0@users.noreply.github.com>
2023-12-09 02:13:14 +0200

    conflicts

--- kern/trap/fault_handler.c
+++ kern/trap/fault_handler.c
@@ -83,55 +83,7 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 		uint32 wsSize = env_page_ws_get_size(curenv);
 #endif
 
-		if(wsSize < (curenv->page_WS_max_size))
-		{
-			//cprintf("PLACEMENT=========================WS Size = %d\n", wsSize );
-			//TODO: [PROJECT'23.MS2 - #15] [3] PAGE FAULT HANDLER - Placement
-			// Write your code here, remove the panic and write your code
-			//panic("page_fault_handler().PLACEMENT is not implemented yet...!!");
-			 struct FrameInfo *f;
-			 uint32 faulted_page  = allocate_frame(&f);
-			 uint32 v_add = ROUNDDOWN(fault_va, PAGE_SIZE);
-			 uint32 perm = pt_get_page_permissions(curenv->env_page_directory, fault_va);
-			 uint32 page = pf_read_env_page(curenv, (void*) fault_va);
-			 if(page != E_PAGE_NOT_EXIST_IN_PF)
-			 {
-				 map_frame(curenv->env_page_directory, f, v_add, PERM_WRITEABLE | PERM_USER);
-				 f->va = v_add;
-			 }
-			 else{
-				 if (((fault_va <= USTACKTOP && fault_va >= USTACKBOTTOM) ||
-					 (fault_va <= USER_HEAP_MAX && fault_va >= USER_HEAP_START)))
-				 {
-						map_frame(curenv->env_page_directory, f, v_add, PERM_WRITEABLE | PERM_USER);
-						f->va = v_add;
-
-				 }else{
-					sched_kill_env(curenv->env_id);
-				 }
-
-			 }
-			  struct WorkingSetElement* new_element = env_page_ws_list_create_element(curenv,fault_va);
-				if (new_element != NULL) {
-					if (curenv->page_last_WS_element == NULL) {
-						curenv->page_last_WS_element= new_element;
-					} else {
-						curenv->page_last_WS_element->prev_next_info.le_next = new_element;
-						curenv->page_last_WS_element = new_element;
-					}
-				}
-				LIST_INSERT_TAIL(&(curenv->page_WS_list), new_element);
-				if (LIST_SIZE(&(curenv->page_WS_list)) == curenv->page_WS_max_size)
-				{
-					curenv->page_last_WS_element = LIST_FIRST(&(curenv->page_WS_list));
-				}
-				else
-				{
-					curenv->page_last_WS_element = NULL;
-				}
-		}
-	else
-	{
+	
 		//cprintf("REPLACEMENT=========================WS Size = %d\n", wsSize );
 		//refer to the project presentation and documentation for details
 		if(isPageReplacmentAlgorithmFIFO())
@@ -150,7 +102,7 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 			struct FrameInfo* new_frame;
 			if((ALSize + SLSize) < (curenv->page_WS_max_size))//one or both of the lists are not full
 			{
-				//TODO: [PROJECT'23.MS3 - #2] [1] PAGE FAULT HANDLER – LRU Placement
+				//TODO: [PROJECT'23.MS3 - #2] [1] PAGE FAULT HANDLER â€“ LRU Placement
 				if(ALSize < (curenv->ActiveListSize))
 				{
 					//active list empty, add element to it
@@ -329,7 +281,7 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 
 			//TODO: [PROJECT'23.MS3 - BONUS] [1] PAGE FAULT HANDLER - O(1) implementation of LRU replacement
 		}
-	}
+	
 }
 
 void __page_fault_handler_with_buffering(struct Env * curenv, uint32 fault_va)
